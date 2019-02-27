c14ed3de0b7e8fbcc24c7890f786faa603ca2a51
v7i2b <v7i2b>
2019-02-26 19:49:13 -0800

    fixed bug in bordercolorpicker, test now pass

--- borderColorPicker.cpp
+++ borderColorPicker.cpp
@@ -10,27 +10,81 @@ borderColorPicker::borderColorPicker(HSLAPixel fillColor, PNG & img, double tole
 
 HSLAPixel borderColorPicker::operator()(int x, int y)
 {
+    if(inBorder(x,y))
+        return color;
+        
     HSLAPixel *original = im.getPixel(x,y);
-    //create vector of pixels that are distance 3 away from x and y
-    vector<HSLAPixel> pixelsWithinThree; 
-    for(int xi = -2; xi < 4; xi++){
-        //cout<<"\n\nxi: "<<xi<<endl;
-        for(int yi = -2; yi < 4; yi++){
+    for(int xi = -2; xi < 3; xi++){
+        for(int yi = -2; yi < 3; yi++){
             int newX = x + xi; 
             int newY = y + yi;
-            if(newX > 0 && newX < im.width() && newY > 0 && newY < im.height()){
-                if(xi != 0 || yi != 0){
-                    HSLAPixel *p = im.getPixel(newX,newY);
-                        bool same = ctr == *p;
-                    if(p->dist(ctr) > tol){
-                        return color; 
-                    }
-                } 
-            }else{
-                return color;
+            if(changeColor(newX,newY)){
+                return color; 
             }
         }
     }
 
+    //check the last four outside pixels 
+    /*
+                    o
+                0 0 0 0 0  
+                0 0 0 0 0  
+              o 0 0 0 0 0 o
+                0 0 0 0 0  
+                0 0 0 0 0
+                    o      
+    */
+   int incre = 3;
+   int newX = x;
+   int newY = y - incre;
+   if(changeColor(newX,newY)){
+        return color; 
+   }
+
+   newY = y + incre;
+   if(changeColor(newX,newY)){
+        return color; 
+   }
+
+   newX = x + incre;
+   newY = y;
+   if(changeColor(newX,newY)){
+       return color; 
+   }
+
+   newX = x - incre;
+   if(changeColor(newX,newY)){
+       return color; 
+   } 
     return *original; 
 }
+
+bool borderColorPicker::inBorder(int x, int y){
+    int w = im.width();
+    int h = im.height();
+    bool top = y >= 0 && y < 3;
+    bool bottom = y < h && y > h-4;
+    bool right = x < w && x > w-4;
+    bool left = x >= 0 && x < 3;
+    return top || bottom || right || left; 
+}
+
+bool borderColorPicker::inFrame(int x, int y){
+    int w = im.width();
+    int h = im.height();
+    bool top = y >= 0;
+    bool bottom = y < h;
+    bool right = x < w;
+    bool left = x >= 0;
+    return top && bottom && right && left; 
+}
+
+ bool borderColorPicker::changeColor(int x, int y){
+
+    if(!inFrame(x,y)) return true; 
+
+    HSLAPixel *p = im.getPixel(x,y);
+    if(p->dist(ctr) > tol) return true; 
+    
+    return false; 
+ }
